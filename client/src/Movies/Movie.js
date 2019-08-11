1b0799f6b28fa1ff8a8a62558c43fb61c7cb69c8
Paul DeYonghe <p.deyonghe@gmail.com>
2019-08-11 17:22:46 -0500

    did the MovieCard stretch; also, added some style

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import MovieCard from './MovieCard'
 
 const Movie = (props) => {
   const [movie, setMovie] = useState();
@@ -10,8 +11,6 @@ const Movie = (props) => {
     const id = props.match.params.id;
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
-
-    console.log("Movie id is: ", id );
 
     axios
       .get(`http://localhost:5000/api/movies/${id}`)
@@ -38,25 +37,7 @@ const Movie = (props) => {
 
   const { title, director, metascore, stars } = movie;
   return (
-    <div className="save-wrapper">
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div>
-      <div className="save-button">Save</div>
-    </div>
+    <MovieCard movie={ movie } />
   );
 }
 
--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,43 @@
 import React from 'react';
+import styled from 'styled-components';
+
+
+const CardStyler = styled.div`
+color: #073964;
+margin: 50px;
+padding: 50px;
+font-size: 150%;
+border: 5px solid lightseagreen;
+background-color: rgba(32, 178, 170, 0.6);
+border-radius: 100px;
+max-width: 1600px;
+font-family: 'Exo', sans-serif;
+`;
 
 const MovieCard = props => {
-  return;
-};
+  return (
+    <div className="save-wrapper">
+      <CardStyler>
+      <div className="movie-card">
+        <h2>{props.movie.title}</h2>
+        <div className="movie-director">
+          Director: <em>{props.movie.director}</em>
+        </div>
+        <div className="movie-metascore">
+          Metascore: <strong>{props.movie.metascore}</strong>
+        </div>
+        <h3>Actors</h3>
+
+        {props.movie.stars.map(star => (
+          <div key={star} className="movie-star">
+            {star}
+          </div>
+        ))}
+      </div>
+      <div className="save-button">Save</div>
+      </CardStyler>
+    </div>
+  );
+}
 
 export default MovieCard;
